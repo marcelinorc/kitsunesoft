a00ac8b671c67da406d3d7a313622b4f1af46e9a
marcelinorc <marcelino.rguez.cancio@gmail.com>
2015-05-08 12:55:55 +0200

    - Changes in the FindRtAudio module

--- sources/dawjo/cmake/Modules/FindRtAudio.cmake
+++ sources/dawjo/cmake/Modules/FindRtAudio.cmake
@@ -1,4 +1,4 @@
-#############################################################################
+###################################################################################
 # 
 # Kitsune Dawjo - Freedom to express
 #
@@ -6,30 +6,43 @@
 # Author:   Marcelino Rodriguez-Cancio <marcelino@kitsunesynth.com>
 # Contents: CMake Find Module
 #
-#############################################################################
-
-#find_package(PkgConfig)
-#pkg_check_modules(PC_LIBXML QUIET libxml-2.0)
-#set(LIBXML2_DEFINITIONS ${PC_LIBXML_CFLAGS_OTHER})
+# Features for this file
+# 1. Be able to find the "standard" distribution of RtAudio that can be 
+#    installed in Linux
+# 2. Be able to link to a custom RtAudio provided by the user (i.e for cross-comp)
+# 3. Be able to link to an static or dynamic version of the program
+# 4. Be able to link to a debug/release version of the library
+#
+#
+# If you want to use this file for a cross compilation, then you should be fine
+# because you should have already set the CMake cross compiler file. In that
+# file the CMAKE_PREFIX is set.
+##################################################################################
 
 #Finds the RtAudio library allowing a paths
 #to include and library passed as parameter to CMAKE
+
 if (RtAudio_INCLUDE_DEFAULT)
   message(STATUS "Using RtAudio includes provided by user") 
   set( RtAudio_INCLUDE_DIR ${RtAudio_INCLUDE_DEFAULT} )
 else ()
-  find_path(RtAudio_INCLUDE_DIR NAMES RtAudio.h)
+  find_path(RtAudio_INCLUDE_DIR NAMES RtAudio.h
+          DOC "The RtAudio include directory")
 endif ()
 
 if (RtAudio_LIBRARY_DEFAULT)
   message(STATUS "Using RtAudio Library provided by user")
   set( RtAudio_LIBRARY ${RtAudio_LIBRARY_DEFAULT} )
 else ()
-  if ( ${RtAudio_LINK_STATIC} )
-    find_library(RtAudio_LIBRARY NAMES libRtAudio_static.a ) #This will not work with the standar package linux
+  if ( RtAudio_LINK_STATIC )
+    find_library(RtAudio_LIBRARY NAMES librtaudio_static.a ) 
   else ()
-    find_library(RtAudio_LIBRARY NAMES libRtAudio.so ) #This will not work with the standar package linux
-  endif ()
+    if ( CMAKE_BUILD_TYPE MATCHES DEBUG )
+      find_library(RtAudio_LIBRARY NAMES librtaudio-dbg.so )    
+    else ()    
+      find_library(RtAudio_LIBRARY NAMES librtaudio.so ) 
+    endif()
+  endif()
 endif ()
 
 message(STATUS "RtAudio Library: ${RtAudio_LIBRARY}")
@@ -43,10 +56,13 @@ if ( RtAudio_LIBRARY AND RtAudio_INCLUDE_DIR )
   set( RtAudio_FOUND 1 )
 endif ()
 
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(RtAudio DEFAULT_MSG RtAudio_LIBRARY RtAudio_INCLUDE_DIR)
+
 # handle the QUIETLY and REQUIRED arguments and set RtAudio_FOUND to TRUE
 # if all listed variables are TRUE
-#include(FindPackageHandleStandardArgs)
-#find_package_handle_standard_args(RtAudio DEFAULT_MSG RtAudio_LIBRARY RtAudio_INCLUDE_DIR)
+#
+#
 #mark_as_advanced(LIBXML2_INCLUDE_DIR LIBXML2_LIBRARY )
 
 
